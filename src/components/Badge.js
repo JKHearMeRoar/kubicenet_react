3eeeaf20402e84daa7b0bfbe5078042d0c4b7071
Jan Kubiče <jan@kubice.net>
2023-10-13 16:50:36 +0200

    Fixed basis bug in Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,40 +3,40 @@ import Icons from "../assets/images/icons.svg";
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const StyledBadge = styled.div`
+	border: 1px solid ${({theme}) => theme.colors.white};
+	flex: 1 1 ${(props) => props.$basis};
+	transition: all 0.5s;
+	font-size: 1rem;
+	font-weight: 400;
+	
+	svg {
+		height: 2rem;
+		width: 2rem;
+		margin-right: 0.5rem;
+	}
+
+	.icon-react {
+		color: #149eca;
+		transition: all 0.5s;
+	}
+
+	&:hover {
+		background-color: ${({theme}) => theme.colors.primary};
+
+		.icon-react {
+			color: ${({theme}) => theme.colors.white};
+		}
+	}
+`;
+
 const Badge = ({ name = 'default', size = '2', title = 'default', alt = 'default', className = '', basis = '40%' }) => {
 	const finalClass = `icon icon-${name} ${className}`;
 	const link = Icons + '#icon-' + name;
 
-	const StyledBadge = styled.div`
-		border: 1px solid ${({theme}) => theme.colors.white};
-		flex: 1 1 ${basis};
-		transition: all 0.5s;
-		font-size: 1rem;
-		font-weight: 400;
-		
-		svg {
-			height: 2rem;
-			width: 2rem;
-			margin-right: 0.5rem;
-		}
-
-		.icon-react {
-			color: #149eca;
-			transition: all 0.5s;
-		}
-
-		&:hover {
-			background-color: ${({theme}) => theme.colors.primary};
-
-			.icon-react {
-				color: ${({theme}) => theme.colors.white};
-			}
-		}
-	`;
-
 	return (
 		<>
-			<StyledBadge className="badge rounded-pill">
+			<StyledBadge className="badge rounded-pill" $basis={basis}>
 				<svg className={finalClass} width={size} height={size}>
 					<title>{alt}</title>
 					<use xlinkHref={link} />
@@ -49,10 +49,11 @@ const Badge = ({ name = 'default', size = '2', title = 'default', alt = 'default
 
 Badge.propTypes = {
 	name: PropTypes.string.isRequired,
-	className: PropTypes.string,
-	alt: PropTypes.string,
 	size: PropTypes.string,
-	title: PropTypes.string
+	title: PropTypes.string,
+	alt: PropTypes.string,
+	className: PropTypes.string,
+	basis: PropTypes.string
 };
 
 export default Badge;
